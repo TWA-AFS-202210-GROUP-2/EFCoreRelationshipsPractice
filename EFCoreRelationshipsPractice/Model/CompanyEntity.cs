42f7644441f6f019cc1b320fef0b9e256ac35f07
xyan8 <xyan8@slb.com>
2022-11-03 16:17:00 +0800

    feat:create many company and successfuly and connect to db

--- EFCoreRelationshipsPractice/Model/CompanyEntity.cs
+++ EFCoreRelationshipsPractice/Model/CompanyEntity.cs
@@ -5,6 +5,7 @@
         public CompanyEntity()
         {
         }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public ProfileEntity? Profile { get; set; }
