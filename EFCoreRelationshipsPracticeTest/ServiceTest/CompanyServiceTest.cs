825238c48fa1d0803efbf4b668e12fdb7e3732cc
Jiahui Xiong <jxiong5@slb.com>
2022-11-03 18:12:43 +0800

    test: add deleteComany test of CompanyService

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -114,13 +114,64 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
                 CertId = "100",
             };
             CompanyService companyService = new CompanyService(context);
-            await companyService.AddCompany(companyDto);
-            var id = context.Companies.First().Id;
+            var id = await companyService.AddCompany(companyDto);
 
             //when
             var resultCompany = await companyService.GetById(id);
             //then
             Assert.Equal(companyDto.Name, resultCompany.Name);
+
+        }
+
+        [Fact]
+        public async Task Should_delete_company_by_id_success_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+
+            CompanyDto companyDto2 = new CompanyDto
+            {
+                Name = "MS",
+                EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Jerry",
+                        Age = 18,
+                    },
+                },
+                ProfileDto = new ProfileDto()
+                {
+                    RegisteredCapital = 100020,
+                    CertId = "101",
+                },
+            };
+            CompanyService companyService = new CompanyService(context);
+            var id = await companyService.AddCompany(companyDto);
+            await companyService.AddCompany(companyDto2);
+
+            //when
+            await companyService.DeleteCompany(id);
+            var getCompaniesAfterDelete =await companyService.GetAll();
+
+            //then
+            Assert.Equal(1, context.Companies.Count());
+            Assert.Equal(companyDto2.Name, getCompaniesAfterDelete[0].Name);
         }
 
         private CompanyDbContext GetCompanyDbContext()
