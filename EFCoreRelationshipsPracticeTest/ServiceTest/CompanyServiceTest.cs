ecc696e9ea1973cf9838f02094094de2a835e6b8
Jiahui Xiong <jxiong5@slb.com>
2022-11-03 18:01:49 +0800

    test: add GetAll and GetById test of CompanyService

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -42,8 +42,85 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
             await companyService.AddCompany(companyDto);
             //then
             Assert.Equal(1,context.Companies.Count());
+        }
 
+        [Fact]
+        public async Task Should_get_all_companies_success_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
 
+            CompanyDto companyDto2 = new CompanyDto
+            {
+                Name = "MS",
+                EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Jerry",
+                        Age = 18,
+                    },
+                },
+                ProfileDto = new ProfileDto()
+                {
+                    RegisteredCapital = 100020,
+                    CertId = "101",
+                },
+            };
+            CompanyService companyService = new CompanyService(context);
+            await companyService.AddCompany(companyDto);
+            await companyService.AddCompany(companyDto2);
+
+            //when
+            await companyService.GetAll();
+
+            //then
+            Assert.Equal(2, context.Companies.Count());
+        }
+
+        [Fact]
+        public async Task Should_get_company_by_id_success_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.EmployeeDtos = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+            CompanyService companyService = new CompanyService(context);
+            await companyService.AddCompany(companyDto);
+            var id = context.Companies.First().Id;
+
+            //when
+            var resultCompany = await companyService.GetById(id);
+            //then
+            Assert.Equal(companyDto.Name, resultCompany.Name);
         }
 
         private CompanyDbContext GetCompanyDbContext()
