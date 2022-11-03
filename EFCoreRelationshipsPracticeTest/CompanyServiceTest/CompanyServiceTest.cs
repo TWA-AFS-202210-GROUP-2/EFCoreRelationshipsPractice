0c9d8f9f81a618a86f59ceb87d1673509bcc6ad8
xyan8 <xyan8@slb.com>
2022-11-03 17:56:23 +0800

    feat: add companyservice test when create company success via company service

--- EFCoreRelationshipsPracticeTest/CompanyServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/CompanyServiceTest/CompanyServiceTest.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EFCoreRelationshipsPractice.Dtos;
+using EFCoreRelationshipsPractice.Repository;
+using EFCoreRelationshipsPractice.Services;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,47 @@ using System.Threading.Tasks;
 
 namespace EFCoreRelationshipsPracticeTest.CompanyServiceTest
 {
-    public class CompanyServiceTest
+    public class CompanyServiceTest : TestBase
     {
+        public CompanyServiceTest(CustomWebApplicationFactory<Program> factory)
+            : base(factory)
+        {
+
+        }
+
+        [Fact]
+        public async void Should_create_company_success_via_company_service()
+        {
+            //given
+            var context = GetCompanyDbContext();
+            CompanyDto companyDto = new CompanyDto();
+            companyDto.Name = "IBM";
+            companyDto.EmployeeDtos = new List<EmployeeDto>
+            {
+                new EmployeeDto()
+                {
+                    Name = "Tom",
+                    Age = 19,
+                },
+            };
+            companyDto.ProfileDto = new ProfileDto()
+            {
+                RegisteredCapital = 100010,
+                CertId = "100",
+            };
+            CompanyService companyService = new CompanyService(context);
+            //when
+            await companyService.AddCompany(companyDto);
+            //then
+            Assert.Equal(1, context.Companies.Count());
+        }
+
+        private CompanyDbContext GetCompanyDbContext()
+        {
+            var scope = Factory.Services.CreateScope();
+            var scopedService = scope.ServiceProvider;
+            CompanyDbContext context = scopedService.GetRequiredService<CompanyDbContext>();
+            return context;
+        }
     }
 }
