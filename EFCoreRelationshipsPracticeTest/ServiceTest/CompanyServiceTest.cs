34a2ac92c2dd82cc08f12c310f6e248edb76f97c
wenruiLi <36104364+wenruiLiA15@users.noreply.github.com>
2022-11-03 17:58:13 +0800

    feat: add getbyid via service test

--- EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
+++ EFCoreRelationshipsPracticeTest/ServiceTest/CompanyServiceTest.cs
@@ -52,8 +52,114 @@ namespace EFCoreRelationshipsPracticeTest.ServiceTest
             CompanyService companyService = new CompanyService(context);
             companyService.AddCompany(companyDto);
             //then
-            Assert.Equal(1,companyService.GetAll().Result.Count);
+            Assert.Equal(1,context.CompanyEntities.Count());
         }
+        [Fact]
+        public async Task Should_return_all_companies_when_getall()
+        {
 
+            //given
+            CompanyDto companyDto1 = new CompanyDto
+            {
+                Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                Profile = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100"
+                },
+            };
+            CompanyDto companyDto2 = new CompanyDto
+            {
+                Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                Profile = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100"
+                },
+            };
+            var context = GetCompanyDbContext();
+            //when
+            CompanyService companyService = new CompanyService(context);
+            companyService.AddCompany(companyDto1);
+            companyService.AddCompany(companyDto2);
+            //then
+            Assert.Equal(2, companyService.GetAll().Result.Count);
+        }
+        [Fact]
+        public async Task Should_get_company_by_id_success_via_service()
+        {
+
+            //given
+            CompanyDto companyDto = new CompanyDto
+            {
+                Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                Profile = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100"
+                },
+            };
+            var context = GetCompanyDbContext();
+            CompanyService companyService = new CompanyService(context);
+            companyService.AddCompany(companyDto);
+            //when
+            var company = companyService.GetById(1);
+            //then
+            Assert.Equal(companyDto.Name,company.Result.Name);
+        }
+        [Fact]
+        public async Task Should_delete_company_success_via_service()
+        {
+
+            //given
+            CompanyDto companyDto = new CompanyDto
+            {
+                Name = "IBM",
+                Employees = new List<EmployeeDto>()
+                {
+                    new EmployeeDto()
+                    {
+                        Name = "Tom",
+                        Age = 19,
+                    },
+                },
+                Profile = new ProfileDto()
+                {
+                    RegisteredCapital = 100010,
+                    CertId = "100"
+                },
+            };
+            var context = GetCompanyDbContext();
+            CompanyService companyService = new CompanyService(context);
+            companyService.AddCompany(companyDto);
+            //when
+
+            //then
+            Assert.Equal(1, context.CompanyEntities.Count());
+        }
     }
 }
